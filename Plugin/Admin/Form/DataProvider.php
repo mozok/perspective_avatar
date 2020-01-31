fa501ae4db1dbc5f1abf4577a8744c8ad2589756
kharidas <kharidas.chebotaryov@gmail.com>
2020-01-31 12:19:07 +0200

    admin image data replacement (modified)

--- Plugin/Admin/Form/DataProvider.php
+++ Plugin/Admin/Form/DataProvider.php
@@ -8,8 +8,25 @@
 
 namespace Perspective\CustomerAvatar\Plugin\Admin\Form;
 
+use Perspective\CustomerAvatar\Api\AvatarManagerInterface;
+use Magento\Customer\Model\Customer\DataProviderWithDefaultAddresses as Subject;
 
 class DataProvider
 {
-
+    public function afterGetData(
+        Subject $subject,
+        $data
+    )
+    {
+        foreach ($data as $customerId => $customerData) {
+            if(@$customerData['customer']) {
+                $accountInfo = $customerData['customer'];
+                if(!$accountInfo[AvatarManagerInterface::ATTRIBUTE_CODE]) {
+                    $generated = $accountInfo[AvatarManagerInterface::ATTRIBUTE_CODE_GENERATED];
+                    $data[$customerId]['customer'][AvatarManagerInterface::ATTRIBUTE_CODE] = $generated;
+                }
+            }
+        }
+        return $data;
+    }
 }
