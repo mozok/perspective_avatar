b2190b8f66398b067804aac2f7228e9949b91326
kharidas <kharidas.chebotaryov@gmail.com>
2020-01-31 12:18:49 +0200

    admin image data replacement

---
+++ Plugin/Admin/Form/DataProvider.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kharidas
+ * Date: 31.01.20
+ * Time: 11:58
+ */
+
+namespace Perspective\CustomerAvatar\Plugin\Admin\Form;
+
+
+class DataProvider
+{
+
+}
